79e9dce47dd4b6a015e1354e5623451b6370c012
YUKI OKUDA <137881758+OKUDAYUKI08@users.noreply.github.com>
2024-06-04 20:27:20 +0900

    修正

--- Assets/Script/TitleScene/BoucingText.cs
+++ Assets/Script/TitleScene/BoucingText.cs
@@ -1,27 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 public class BoucingText : MonoBehaviour
 {
+    public RectTransform canvas;
+
+    private float[] uisize_x;
+    private float[] uisize_y;
 
     public TextMeshProUGUI[] uiText;  // ここにTextオブジェクトをアタッチします
     public float speed = 100f;
 
     private RectTransform[] rectTransform;
-    private Vector2 direction = new Vector2 (1,1);
+    private Vector2[] direction;
 
-    public float ScreenWidth;
-    public float ScreenHeight;
+
+    private float ScreenWidth;
+    private float ScreenHeight;
 
     void Start()
     {
-        // for(int i=0;i<uiText.Length; i++)
-        // {
-        //     rectTransform[i] = uiText[i].GetComponent<RectTransform>();
-        //     Debug.Log("aaa");
-        // }
-        rectTransform[1]=uiText[1].GetComponent<RectTransform>();
+        ScreenWidth=(canvas.rect.size.x)/2;
+        ScreenHeight=(canvas.rect.size.y)/2;
+        direction=new Vector2[uiText.Length];
+        rectTransform=new RectTransform[uiText.Length];
+        uisize_x=new float[uiText.Length];
+        uisize_y=new float[uiText.Length];
+
+        for(int i=0;i<uiText.Length; i++)
+        {
+            rectTransform[i] = uiText[i].GetComponent<RectTransform>();
+            uisize_x[i]=rectTransform[i].rect.size.x/2;
+            uisize_y[i]=rectTransform[i].rect.size.y/2;
+        }
+
+
+        for(int i=0;i<uiText.Length; i++)
+        {
+            direction[i]=new Vector2(Random.Range(0, 2) * 2 - 1,Random.Range(0, 2) * 2 - 1);
+        }
     }
 
     void Update()
@@ -29,18 +48,18 @@ public class BoucingText : MonoBehaviour
         // テキストの位置を更新
         for(int i=0;i<uiText.Length;i++)
         {
-            rectTransform[i].localPosition += (Vector3)direction * speed * Time.deltaTime;
+            rectTransform[i].localPosition += (Vector3)direction[i] * speed * Time.deltaTime;
 
             // テキストがCanvasの端に到達したら方向を反転
-            if (rectTransform[i].localPosition.x > ScreenWidth || rectTransform[i].localPosition.x < -ScreenWidth)
+            if (rectTransform[i].localPosition.x > ScreenWidth-uisize_x[i]|| rectTransform[i].localPosition.x < -ScreenWidth+uisize_x[i])
             {
-                direction.x = -direction.x;
+                direction[i].x = -direction[i].x;
                 ChangeTextColor(uiText[i]);
             }
 
-            if (rectTransform[i].localPosition.y > ScreenHeight || rectTransform[i].localPosition.y < -ScreenHeight)
+            if (rectTransform[i].localPosition.y > ScreenHeight-uisize_y[i]|| rectTransform[i].localPosition.y < -ScreenHeight+uisize_y[i])
             {
-                direction.y = -direction.y;
+                direction[i].y = -direction[i].y;
                 ChangeTextColor(uiText[i]);
             }
         }
--- Assets/Script/TitleScene/BouncingButton.cs
+++ Assets/Script/TitleScene/BouncingButton.cs
@@ -5,42 +5,58 @@ using UnityEngine.UI;
 
 public class BouncingButton : MonoBehaviour
 {
+    public RectTransform canvas;
+
+    private float[] uisize_x;
+
+    private float[] uisize_y;
     public Button[] uibutton;  // ここにTextオブジェクトをアタッチします
     public float speed = 100f;
 
     private RectTransform[] rectTransform;
-    private Vector2 direction = new Vector2 (1,1);
+    private Vector2[] direction ;
 
-    public float ScreenWidth;
-    public float ScreenHeight;
+    private float ScreenWidth;
+    private float ScreenHeight;
+
+
 
     void Start()
     {
-        // for(var i=0;i<uibutton.Length;i++){
-        //     rectTransform[i]=uibutton[i].GetComponent<RectTransform>();
-        // }
-        rectTransform[0]=uibutton[0].GetComponent<RectTransform>();
-        rectTransform[1]=uibutton[1].GetComponent<RectTransform>();
+        ScreenWidth=(canvas.rect.size.x)/2;
+        ScreenHeight=(canvas.rect.size.y)/2;
+        rectTransform = new RectTransform[uibutton.Length];
+        uisize_x=new float[uibutton.Length];
+        uisize_y=new float[uibutton.Length];
+        direction=new Vector2[uibutton.Length];
+        for(var i=0;i<uibutton.Length;i++){
+            rectTransform[i]=uibutton[i].GetComponent<RectTransform>();
+            uisize_x[i]=rectTransform[i].rect.size.x/2;
+            uisize_y[i]=rectTransform[i].rect.size.y/2;
+        }
+        for(int i=0;i<uibutton.Length; i++)
+        {
+            direction[i]=new Vector2(Random.Range(0, 2) * 2 - 1,Random.Range(0, 2) * 2 - 1);
+        }
     }
 
     void Update()
     {
-        // テキストの位置を更新
+        //テキストの位置を更新
         for(var i = 0; i < uibutton.Length ; i++)
         {
-            rectTransform[i].localPosition += (Vector3)direction * speed * Time.deltaTime;
+            rectTransform[i].localPosition += (Vector3)direction[i] * speed * Time.deltaTime;
 
             // テキストがCanvasの端に到達したら方向を反転
-            if (rectTransform[i].localPosition.x > ScreenWidth || rectTransform[i].localPosition.x < -ScreenWidth)
+            if (rectTransform[i].localPosition.x > ScreenWidth-uisize_x[i] || rectTransform[i].localPosition.x < -ScreenWidth+uisize_x[i])
             {
-                direction.x = -direction.x;
+                direction[i].x = -direction[i].x;
             }
 
-            if (rectTransform[i].localPosition.y > ScreenHeight || rectTransform[i].localPosition.y < -ScreenHeight)
+            if (rectTransform[i].localPosition.y > ScreenHeight-uisize_y[i] || rectTransform[i].localPosition.y < -ScreenHeight+uisize_y[i])
             {
-                direction.y = -direction.y;
+                direction[i].y = -direction[i].y;
             }
         }
-
     }
 }
